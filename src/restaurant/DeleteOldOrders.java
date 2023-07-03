d7774b255734a72dcf530c54b04453d5a6e07857
alexanderik79 <alexanderik79@gmail.com>
2023-07-03 19:03:15 +0200

    навел порядок убрал лишнее добавил интерфейсы работникам ресторана

--- src/restaurant/DeleteOldOrders.java
+++ src/restaurant/DeleteOldOrders.java
@@ -4,9 +4,7 @@ import java.io.File;
 
 public class DeleteOldOrders {
     public void deleteOldOrdersKitchen (){
-
         File folder = new File("src/resKitchen/");
-        // сканируем все директории в папке resources и закидываем их пути в list
         for (File file : folder.listFiles())
         {
             if (file.delete()){
@@ -16,12 +14,11 @@ public class DeleteOldOrders {
     }
     public void deleteOldOrdersBar (){
         File folder = new File("src/resBar/");
-        // сканируем все директории в папке resources и закидываем их пути в list
         for (File file : folder.listFiles())
         {
             if (file.delete()){
                 System.out.println("Cleaning Bar");
             }
         }
-}
+    }
 }
